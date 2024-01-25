bb97b63cfb347134fcc39a63e90ba6af634e7bd9
nunomourao <157617077+nunomourao@users.noreply.github.com>
2024-01-25 18:39:41 -0500

    again

--- the_sea.js
+++ the_sea.js
@@ -16,6 +16,7 @@ let currentPage = -1; // Variable to keep track of the current page
 let expanding = false; // Variable to track if the red square is expanding
 let expandedSize = buttonSize * 6; // Size of the expanded rectangle
 let expandedImage;
+let fadeAlpha = 0; // Variable to control fading effect
 
 function preload() {
   expandedImage = loadImage('image.jpg');
@@ -34,8 +35,8 @@ function draw() {
   xOffset += (targetXOffset - xOffset) * easing;
   yOffset += (targetYOffset - yOffset) * easing;
 
-  let centerX = windowWidth / 2;
-  let centerY = windowHeight / 2;
+  let centerX = windowWidth / 2 + scrollX;
+  let centerY = windowHeight / 2 + scrollY;
 
   let yoff = yOffset;
 
@@ -80,6 +81,7 @@ function draw() {
           if (mouseIsPressed) {
             currentPage = x + y * cols; // Calculate the current page based on button position
             expanding = true;
+            fadeAlpha = 0; // Reset fadeAlpha when the red square is clicked
           }
         } else {
           fill(0); // Default black color for buttons
@@ -91,11 +93,16 @@ function draw() {
         noStroke();
         rect(bx, by, buttonSize, buttonSize);
 
-        // Display the expanded image when red square is clicked
+        // Display the expanded image with fading effect when the red square is clicked
         if (expanding && currentPage === x + y * cols) {
           let expandedX = bx - (expandedSize - buttonSize) / 2 - expandedSize * 0.2 + centerX;
           let expandedY = by - (expandedSize - buttonSize) / 2 + centerY;
+          
+          fill(255, 0, 0, fadeAlpha); // Set fill with fading effect
           image(expandedImage, expandedX, expandedY, expandedSize * 1.5, expandedSize); // Display the loaded image
+
+          // Gradually increase alpha for fading effect
+          fadeAlpha = min(fadeAlpha + 5, 255);
         }
       }
       xoffButtons += noiseScale;
@@ -105,8 +112,8 @@ function draw() {
 }
 
 function mouseMoved() {
-  targetXOffset = map(mouseX + scrollX, 0, windowWidth, -w / 300, w / 300);
-  targetYOffset = map(mouseY + scrollY, 0, windowHeight, -h / 300, h / 300);
+  targetXOffset = map(mouseX, 0, windowWidth, -w / 300, w / 300);
+  targetYOffset = map(mouseY, 0, windowHeight, -h / 300, h / 300);
 
   // Reset currentPage when the mouse is not hovering over any button
   if (!expanding) {
